1e9c2d19aa13ac435bf672b0ccb68e6f9d7004f6
Leave-On <sarkisyanrnd@gmail.com>
2023-02-11 17:05:14 +0300

    add not found page, loader component

---
+++ build/582.85840dc8a2e9dd50eae6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[582],{582:(e,t,r)=>{r.r(t),r.d(t,{default:()=>n});var c=r(893),u=r(887);const n=function(){var e=(0,u.$G)("about").t;return(0,c.jsx)("div",{children:e("О нас")})}}}]);
