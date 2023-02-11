a8e12b9caeaba699608500db9a624741337306b2
Leave-On <sarkisyanrnd@gmail.com>
2023-02-11 17:15:29 +0300

    Removed build folder from repository

--- build/986.07448869312b4f877514.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[986],{986:(e,r,t)=>{t.r(r),t.d(r,{default:()=>s});var c=t(893),n=t(887);const s=function(){var e=(0,n.$G)().t;return(0,c.jsx)("div",{children:e("Главная страница")})}}}]);
