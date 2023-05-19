df7d4639180a9af7dfceb7fd26ff05cc6b9eb5ac
Leave-On <sarkisyanrnd@gmail.com>
2023-05-19 10:49:39 +0300

    add argument for build selector helper

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -2,73 +2,66 @@ import { classNames } from '@/shared/lib/classNames/classNames';
 import { Text, TextSize } from '@/shared/ui/Text';
 import { HTMLAttributeAnchorTarget, memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ArticleView } from "../../model/consts/consts";
+import { ArticleView } from '../../model/consts/consts';
 import { Article } from '../../model/types/article';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 import cls from './ArticleList.module.scss';
 
 interface ArticleListProps {
-   className?: string;
-   articles: Article[];
-   isLoading?: boolean;
-   view?: ArticleView;
-   target?: HTMLAttributeAnchorTarget;
+	className?: string;
+	articles: Article[];
+	isLoading?: boolean;
+	view?: ArticleView;
+	target?: HTMLAttributeAnchorTarget;
 }
 
 const getSkeletons = (view: ArticleView) => {
-    return new Array(view === ArticleView.BLOCKS ? 9 : 3)
-        .fill(0)
-        .map((i, index) => (
-            <ArticleListItemSkeleton
-                view={view}
-                key={index}
-            />
-        ))
-
-}
-
+	return new Array(view === ArticleView.BLOCKS ? 9 : 3)
+		.fill(0)
+		.map((i, index) => <ArticleListItemSkeleton view={view} key={index} />);
+};
 
 export const ArticleList = memo((props: ArticleListProps) => {
-    const {
-        className,
-        articles,
-        isLoading,
-        target,
-        view = ArticleView.BLOCKS
-    } = props;
-    const { t } = useTranslation()
+	const {
+		className,
+		articles,
+		isLoading,
+		target,
+		view = ArticleView.BLOCKS,
+	} = props;
+	const { t } = useTranslation();
 
-    const renderArticle = (article: Article) => {
-        return (
-            <ArticleListItem
-                article={article}
-                view={view}
-                className={cls.card}
-                key={article.id}
-                target={target}
-            />)
-    }
+	const renderArticle = (article: Article) => {
+		return (
+			<ArticleListItem
+				article={article}
+				view={view}
+				className={cls.card}
+				key={article.id}
+				target={target}
+			/>
+		);
+	};
 
+	if (!isLoading && !articles.length) {
+		return (
+			<div className={classNames(cls.ArticleList, {}, [className, cls[view]])}	>
+				<Text
+					size={TextSize.L}
+					title={t('Articles not found') as string}
+				/>
+			</div>
+		);
+	}
 
-    if(!isLoading && !articles.length) {
-        return (
-            <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-                <Text size={TextSize.L} title={t('Articles not found') as string} />
-            </div>
-        )
-    }
-
-    return (
-        <div
-            className={classNames(cls.ArticleList, {}, [className, cls[view]])}
-            data-testid='ArticleList'
-        >
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null
-            }
-            {isLoading && getSkeletons(view)}
-        </div>
-    );
-})
+	return (
+		<div
+			className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+			data-testid="ArticleList"
+		>
+			{articles.length > 0 ? articles.map(renderArticle) : null}
+			{isLoading && getSkeletons(view)}
+		</div>
+	);
+});
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,5 +1,8 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
+import {
+    AnimationProvider,
+    useAnimationLibs
+} from '@/shared/lib/components/AnimationProvider';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { memo, ReactNode, useCallback, useEffect } from 'react';
 import { Overlay } from '../Overlay/Overlay';
@@ -7,107 +10,113 @@ import { Portal } from '../Portal/Portal';
 import cls from './Drawer.module.scss';
 
 interface DrawerProps {
-  className?: string;
-  children: ReactNode;
-  isOpen?: boolean;
-  onClose?: () => void;
-  lazy?: boolean;
+	className?: string;
+	children: ReactNode;
+	isOpen?: boolean;
+	onClose?: () => void;
+	lazy?: boolean;
 }
 
 const height = window.innerHeight - 100;
 
 export const DrawerContent = memo((props: DrawerProps) => {
-    const { Spring, Gesture } = useAnimationLibs()
-    const [{ y }, api] = Spring.useSpring(() => ({ y: height }))
-    const {
-        className,
-        children,
-        isOpen,
-        onClose,
-        lazy
-    } = props
-    const { theme } = useTheme()
-    const openDrawer = useCallback(() => {
-        api.start({ y: 0, immediate: false });
-    }, [api]);
+	const { Spring, Gesture } = useAnimationLibs();
+	const [{ y }, api] = Spring.useSpring(() => ({ y: height }));
+	const { className, children, isOpen, onClose, lazy } = props;
+	const { theme } = useTheme();
+	const openDrawer = useCallback(() => {
+		api.start({ y: 0, immediate: false });
+	}, [api]);
 
-    useEffect(() => {
-        if (isOpen) {
-            openDrawer();
-        }
-    }, [api, isOpen, openDrawer]);
+	useEffect(() => {
+		if (isOpen) {
+			openDrawer();
+		}
+	}, [api, isOpen, openDrawer]);
 
-    const close = (velocity = 0) => {
-        api.start({
-            y: height,
-            immediate: false,
-            config: { ...Spring.config.stiff, velocity },
-            onResolve: onClose,
-        });
-    };
+	const close = (velocity = 0) => {
+		api.start({
+			y: height,
+			immediate: false,
+			config: { ...Spring.config.stiff, velocity },
+			onResolve: onClose,
+		});
+	};
 
-    const bind = Gesture.useDrag(
-        ({
-            last,
-            velocity: [, vy],
-            direction: [, dy],
-            movement: [, my],
-            cancel,
-        }) => {
-            if (my < -70) cancel();
+	const bind = Gesture.useDrag(
+		({
+			last,
+			velocity: [, vy],
+			direction: [, dy],
+			movement: [, my],
+			cancel,
+		}) => {
+			if (my < -70) cancel();
 
-            if (last) {
-                if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
-                    close();
-                } else {
-                    openDrawer();
-                }
-            } else {
-                api.start({ y: my, immediate: true });
-            }
-        },
-        {
-            from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
-        },
-    );
+			if (last) {
+				if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+					close();
+				} else {
+					openDrawer();
+				}
+			} else {
+				api.start({ y: my, immediate: true });
+			}
+		},
+		{
+			from: () => [0, y.get()],
+			filterTaps: true,
+			bounds: { top: 0 },
+			rubberband: true,
+		},
+	);
 
-    if (!isOpen) {
-        return null;
-    }
+	if (!isOpen) {
+		return null;
+	}
 
-    const display = y.to((py) => (py < height ? 'block' : 'none'));
+	const display = y.to((py) => (py < height ? 'block' : 'none'));
 
-    return (
-        <Portal>
-            <div className={classNames(cls.Drawer, {}, [className, theme, 'app_drawer'])}>
-                <Overlay onClick={close} />
-                <Spring.a.div
-                    className={cls.sheet}
-                    style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
-                    {...bind()}
-                >
-                    {children}
-                </Spring.a.div>
-            </div>
-        </Portal>
-    );
+	return (
+		<Portal>
+			<div
+				className={classNames(cls.Drawer, {}, [
+					className,
+					theme,
+					'app_drawer',
+				])}
+			>
+				<Overlay onClick={close} />
+				<Spring.a.div
+					className={cls.sheet}
+					style={{
+						display,
+						bottom: `calc(-100vh + ${height - 100}px)`,
+						y,
+					}}
+					{...bind()}
+				>
+					{children}
+				</Spring.a.div>
+			</div>
+		</Portal>
+	);
 });
 
 const DrawerAsync = (props: DrawerProps) => {
-    const { isLoaded } = useAnimationLibs()
+	const { isLoaded } = useAnimationLibs();
 
-    if(!isLoaded) {
-        return null
-    }
+	if (!isLoaded) {
+		return null;
+	}
 
-    return <DrawerContent {...props}/>
-}
+	return <DrawerContent {...props} />;
+};
 
 export const Drawer = (props: DrawerProps) => {
-
-    return (
-        <AnimationProvider>
-            <DrawerAsync {...props}/>
-        </AnimationProvider>
-    )
-}
+	return (
+		<AnimationProvider>
+			<DrawerAsync {...props} />
+		</AnimationProvider>
+	);
+};
